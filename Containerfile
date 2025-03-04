5bbfe802552959503236b0f33f2c3a907a0aad87
Janik Knodel <10290002+led0nk@users.noreply.github.com>
2025-03-04 11:33:00 +0100

    add Containerfile

---
+++ Containerfile
@@ -0,0 +1,17 @@
+FROM golang:1.22
+
+LABEL org.opencontainers.image.source=https://github.com/led0nk/eip-server
+
+COPY . /go/src/github.com/led0nk/eip-server
+
+WORKDIR /go/src/github.com/led0nk/eip-server
+
+RUN CGO_ENABLED=0 go build -v -o /eip-server cmd/server/main.go
+
+FROM scratch
+
+COPY --from=0 /eip-server /eip-server
+
+EXPOSE 8080
+
+CMD ["/eip-server", "--booltags=5", "--inttags=5"]
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"math/rand"
 	"os"
 	"time"
 
@@ -11,13 +12,18 @@ import (
 )
 
 func main() {
-	booltags := flag.Int("booltags", 10, "Number of boolean tags to create")
-	path := flag.String("path", "1,0", "Path to read data from")
+	var (
+		booltags = flag.Int("booltags", 10, "Number of boolean tags to create")
+		inttags  = flag.Int("inttags", 10, "Number of integer tags to create")
+		path     = flag.String("path", "1,0", "Path to read data from")
+	)
 	flag.Parse()
 
 	r := gologix.PathRouter{}
 
-	mtp := gologix.MapTagProvider{}
+	mapTagProvider := gologix.MapTagProvider{
+		Data: make(map[string]any),
+	}
 
 	path1, err := gologix.ParsePath(*path)
 	if err != nil {
@@ -25,29 +31,41 @@ func main() {
 		os.Exit(1)
 	}
 
-	r.Handle(path1.Bytes(), &mtp)
+	r.Handle(path1.Bytes(), &mapTagProvider)
 
 	s := gologix.NewServer(&r)
 
-	createTags(&mtp, *booltags)
+	createTags(&mapTagProvider, *booltags, *inttags)
 
-	go s.Serve()
+	go func(s *gologix.Server) {
+		err := s.Serve()
+		if err != nil {
+			log.Fatalf("Failed to run server: %v", err)
+			panic(err)
+		}
+	}(s)
 
 	t := time.NewTicker(time.Second * 5)
 	for {
 		<-t.C
-		mtp.Mutex.Lock()
-		log.Printf("Data 1: %v", mtp.Data)
-		mtp.Mutex.Unlock()
+		mapTagProvider.Mutex.Lock()
+		log.Printf("Data 1: %v", mapTagProvider.Data)
+		mapTagProvider.Mutex.Unlock()
 
 	}
 }
 
-func createTags(mtp *gologix.MapTagProvider, booltags int) {
+func createTags(mtp *gologix.MapTagProvider, booltags int, inttags int) {
 	mtp.Mutex.Lock()
 	defer mtp.Mutex.Unlock()
 	for i := 1; i <= booltags; i++ {
 		name := fmt.Sprintf("Bool%d", i)
 		mtp.Data[name] = true
+	}
+
+	for i := 1; i <= inttags; i++ {
+		name := fmt.Sprintf("Int%d", i)
+		rndInt := rand.Intn(1000)
+		mtp.Data[name] = rndInt
 	}
 }
